b4edad4db75e2f1525863ae1f9d5e24cfb11eb18
Omar Rodriguez Herrera <shepeni@gmail.com>
2019-03-26 10:56:59 -0600

    Initial commit, created application

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Omar Rodriguez Herrera on 24/03/19.
+//  Copyright © 2019 Omar Rodriguez Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
